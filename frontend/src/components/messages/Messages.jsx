d17c26220817073c28ad02e6756688b9b8dd7826
Paramjit singh <singh.param1102@gmail.com>
2024-06-05 14:23:12 +0530

    add

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,11 +1,12 @@
 import { useGetMessages } from "../../hooks/useGetMessages.js";
+import MessageSkeleton from "../skeletons/MessageSkeleton.jsx";
 import Message from "./Message.jsx";
 function Messages() {
   const { messages, loading } = useGetMessages();
   console.log(messages, "messgae");
   return (
     <div className="px-4 flex-1 overflow-auto">
-    
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
     </div>
   );
 }
--- frontend/src/components/skeletons/MessageSkeleton.jsx
+++ frontend/src/components/skeletons/MessageSkeleton.jsx
@@ -1,6 +1,23 @@
-<div className="flex flex-col gap-4 w-52">
-  <div className="skeleton h-32 w-full"></div>
-  <div className="skeleton h-4 w-28"></div>
-  <div className="skeleton h-4 w-full"></div>
-  <div className="skeleton h-4 w-full"></div>
-</div>
+import React from "react";
+
+const MessageSkeleton = () => {
+  return (
+    <>
+      <div className="flex  gap-3 items-center">
+        <div className="skeleton w-10 h-10 rounded-fill shrink-0"></div>
+        <div className="flex flex-col gap-1">
+          <div className="skeleton h-4 w-40"></div>
+          <div className="skeleton h-4 w-40"></div>
+        </div>
+      </div>
+      <div className="flex gap-3 items-center justify-normal">
+        <div className="flex flex-col gap-1">
+          <div className="skeleton h-4 w-40"></div>
+        </div>
+        <div className="skeleton w-40 h-10 rounded-full shrink-0"></div>
+      </div>
+    </>
+  );
+};
+
+export default MessageSkeleton;
