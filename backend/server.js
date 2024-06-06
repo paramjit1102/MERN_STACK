32d79e81e0dc14f124dd7435d18c387df351c755
Paramjit singh <singh.param1102@gmail.com>
2024-06-06 18:24:48 +0530

    d

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,11 @@ app.use(urlencodedParser)
 app.use(cors())
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(cookieParser())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use('/api/auth', authroutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
@@ -30,11 +35,7 @@ app.use(express.static(path.join(__dirname, '/frontend/dist')))
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 })
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 connectDB()
     .then(() => {
         server.listen(process.env.PORT || 5000, (res, err) => {
