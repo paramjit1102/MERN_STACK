4083fbf8fbb37fa08e43138aae7a59c2805f16eb
Paramjit singh <singh.param1102@gmail.com>
2024-06-07 09:21:05 +0530

    orr

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -4,12 +4,7 @@ import express from 'express';
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: ['http://localhost:3000'],
-        methods: ['get', 'post']
-    }
-})
+const io = new Server(server)
 
 export const getReceiverSocketId=(receiverId)=>{
     return userSocketMap[receiverId];
