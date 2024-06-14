cd117054a5614c6480af6d9bcf7ad7ad36514ffc
Paramjit Singh <paramjit.offsureit@gmail.com>
2024-06-14 16:03:51 +0530

    change daylight

--- frontend/README.md
+++ frontend/README.md
@@ -6,3 +6,6 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+when project is local then change the url from three files i.e  vite.config.js, context/socketContext.jsx,backend socket/socket.js
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,27 +4,47 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import { Toaster } from "react-hot-toast";
+// import { IoSunnyOutline } from "react-icons/io5";
 import { useAuthContext } from "./context/AuthContent";
 function App() {
   const { authUser } = useAuthContext();
+  // let bg = true;
+  // const changeStyle = () => {
+  //   if (bg) {
+  //     // document.body.style.backgroundColor = "#2f4f4f";
+  //     document.body.style.backgroundImage = "url('bg.png')";
+  //     return (bg = false);
+  //   }
+  //   // document.body.style.backgroundColor = "#000000";
+
+  //   document.body.style.backgroundImage = "url('bn.png')";
+  //   return (bg = true);
+  // };
   return (
-    <div className="p-4 h-screen flex items-center justify-center">
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <Home /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/login"
-          element={authUser ? <Navigate to="/" /> : <Login />}
-        />
-        <Route
-          path="/signup"
-          element={authUser ? <Navigate to="/" /> : <Signup />}
-        />
-      </Routes>
-      <Toaster />
-    </div>
+    <>
+      <div className="p-4 h-screen flex  items-center justify-center">
+        {/* <div className="">
+          <button onClick={changeStyle}>
+            <IoSunnyOutline />  
+          </button>
+        </div> */}
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={authUser ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to="/" /> : <Signup />}
+          />
+        </Routes>
+        <Toaster />
+      </div>
+    </>
   );
 }
 
